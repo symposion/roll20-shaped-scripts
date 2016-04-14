8e66e930fc8556c05957138b6d5380efa4fa1636
Lucian Holland <lucian@symposion.co.uk>
2016-04-14 20:50:23 +0200

    chore(build): Introduce commit hooks and automatic semver releases
    Moved to using conventional commits and auto-generation of release from travis based on the commit
    messages. Also added commit hooks to make life easier for new committers

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
+'use strict';
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var webpack = require('webpack-stream');
+var tagVersion = require('gulp-tag-version');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+var addSrc = require('gulp-add-src');
+var conventionalChangelog = require('gulp-conventional-changelog');
+var conventionalRecommendedBump = require('conventional-recommended-bump');
 
 gulp.task('default', ['test', 'lint'], function () {
-  'use strict';
   return gulp.src('./lib/entry-point.js')
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('lint', function () {
-  'use strict';
   return gulp.src('./lib/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
@@ -19,7 +24,29 @@ gulp.task('lint', function () {
 });
 
 gulp.task('test', function () {
-  'use strict';
   return gulp.src('test/test-*.js', { read: false })
     .pipe(mocha());
 });
+
+gulp.task('release', ['changelog'], function (done) {
+  conventionalRecommendedBump({ preset: 'angular' }, function (err, importance) {
+    // Get all the files to bump version in
+    gulp.src('./package.json')
+      .pipe(bump({ type: importance }))
+      .pipe(gulp.dest('./'))
+      .pipe(addSrc.append('./CHANGELOG.md'))
+      .pipe(git.commit('chore(release): bump package version and update changelog', { emitData: true }))
+      // **tag it in the repository**
+      .pipe(tagVersion());
+    done(err);
+  });
+
+
+});
+
+gulp.task('changelog', function () {
+  return gulp.src('./CHANGELOG.md', { buffer: false })
+    .pipe(conventionalChangelog({ preset: 'angular' }))
+    .pipe(gulp.dest('./'));
+});
+
