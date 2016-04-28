77bb76cef5716b84bd93ec5260ed6ce6261cff45
Lucian Holland <lucian@symposion.co.uk>
2016-04-28 23:55:35 +0200

    fix(config): Fix upgrade crash
    fixes #85

---
+++ test/test-migrations.js
@@ -0,0 +1,76 @@
+/* globals describe: false, it:false */
+'use strict';
+const expect = require('chai').expect;
+const Migrator = require('../lib/migrations.js');
+const dl = require('./dummy-logger');
+
+
+describe('Migrator', function () {
+  it('fails for missing version', function () {
+    const mig = new Migrator();
+    const config = {};
+    mig.nextVersion().addProperty('test', 'testVal');
+
+    expect(function () {
+      mig.migrateConfig(config, dl);
+    }).to.throw(Error);
+  });
+
+  it('migrates correctly', function () {
+    const mig = new Migrator();
+    const config = {
+      version: 0.1,
+      foo: 'foo',
+      bar: 'bar',
+      blort: {
+        wibble: 'test',
+        wibblePrime: 'test2',
+      },
+    };
+    mig.nextVersion()
+      .addProperty('newProp', 'newValue')
+      .moveProperty('blort.wibble', 'newChild.newGrandChild')
+      .nextVersion()
+      .moveProperty('foo', 'blort.foo');
+
+    expect(mig.migrateConfig(config, dl)).to.deep.equal({
+      version: 0.3,
+      bar: 'bar',
+      blort: {
+        wibblePrime: 'test2',
+        foo: 'foo',
+      },
+      newProp: 'newValue',
+      newChild: {
+        newGrandChild: 'test',
+      },
+    });
+  });
+
+  it('upgrades old Shaped Config correctly', function () {
+    const config = {
+      version: 0.1,
+      genderPronouns: [
+        'someStuff',
+      ],
+    };
+
+    expect(Migrator.migrateShapedConfig(config, dl).genderPronouns).to.have.lengthOf(3);
+  });
+
+  it('upgrades recent Shaped Config correctly', function () {
+    const config = {
+      version: 0.1,
+      newCharSettings: {
+        savingThrowsHalfProf: false,
+        mediumArmorMaxDex: 2,
+      },
+    };
+
+    const result = Migrator.migrateShapedConfig(config, dl);
+    expect(result.newCharSettings).to.have.property('houserules');
+    expect(result.newCharSettings.houserules).to.have.property('savingThrowsHalfProf', false);
+    expect(result.newCharSettings.houserules).to.have.property('mediumArmorMaxDex', 2);
+    expect(result.newCharSettings).not.to.have.property('savingThrowsHalfProf');
+  });
+});
