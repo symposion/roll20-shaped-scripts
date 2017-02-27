835ee0594fd4cca814206f25b699940faf5afb62
Lucian <lucian@symposion.co.uk>
2017-02-27 18:09:58 +0100

    feat(config): Ask user before  upgrading + cmd to wipe config closes #279

--- test/test-migrations.js
+++ test/test-migrations.js
@@ -1,7 +1,7 @@
 /* globals describe: false, it:false */
 'use strict';
 const expect = require('chai').expect;
-const Migrator = require('../lib/migrations.js');
+const Migrator = require('../lib/migrator.js');
 const dl = require('./dummy-logger');
 
 
@@ -46,36 +46,4 @@ describe('Migrator', function () {
       },
     });
   });
-
-  it('upgrades old Shaped Config correctly', function () {
-    const state = {
-      version: 0.1,
-      config: {
-        genderPronouns: [
-          'someStuff',
-        ],
-      },
-    };
-
-    expect(Migrator.migrateShapedConfig(state, dl).config.genderPronouns).to.have.lengthOf(3);
-  });
-
-  it('upgrades recent Shaped Config correctly', function () {
-    const config = {
-      version: 0.1,
-      config: {
-        newCharSettings: {
-          savingThrowsHalfProf: false,
-          mediumArmorMaxDex: 2,
-        },
-      },
-    };
-
-    const result = Migrator.migrateShapedConfig(config, dl).config;
-    expect(result.newCharSettings).to.have.property('houserules');
-    expect(result.newCharSettings.houserules.saves).to.have.property('savingThrowsHalfProf', false);
-    expect(result.newCharSettings.houserules).to.have.property('mediumArmorMaxDex', 2);
-    expect(result.newCharSettings).not.to.have.property('savingThrowsHalfProf');
-  });
-})
-;
+});
